822fd8242252d0144f4636aec81f012e6dd845f1
nikolaytsvyatkov <nikolay.tsvyatkov99@gmail.com>
2021-02-12 12:24:37 +0200

    Feature - updating and deleting a offer - completed!

--- src/main/java/com/example/mobileapp/web/OfferController.java
+++ src/main/java/com/example/mobileapp/web/OfferController.java
@@ -13,9 +13,12 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -43,44 +46,82 @@ public class OfferController {
     @GetMapping("/add")
     public String addOffer(Model model,
                            HttpSession session,
-                           RedirectAttributes redirectAttributes) {
+                           RedirectAttributes redirectAttributes,
+                           @RequestParam(required = false, name = "mode") String edit,
+                           @RequestParam(required = false) Long offerId) {
 
         if (session.getAttribute("user") == null) {
             redirectAttributes.addAttribute("redirectUrl", "/offers/add");
             return "redirect:/auth/login";
         }
 
-        if (!model.containsAttribute("offer")) {
-            model.addAttribute("offer", new Offer());
+
+        if (edit == null) {
+            if (!model.containsAttribute("offer")) {
+                model.addAttribute("offer", new Offer());
+            }
+        } else {
+            model.addAttribute("offer", this.offerService.getOfferById(offerId));
         }
+
 
         model.addAttribute("brands", this.brandService.getBrands());
         return "offer-add";
     }
 
     @PostMapping("/add")
-    public String addNewOffer(@Valid @ModelAttribute Offer offer,
+    public String addNewOffer(@Valid @ModelAttribute("offer") Offer offer,
                               BindingResult bindingResult,
                               RedirectAttributes redirectAttributes,
                               HttpSession httpSession) {
 
         offer.setSeller((User) httpSession.getAttribute("user"));
         System.out.println();
-       if (bindingResult.hasErrors()) {
-           redirectAttributes.addFlashAttribute("offer", offer);
-           redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.offer", bindingResult);
-           return "redirect:add";
-       }
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("offer", offer);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.offer", bindingResult);
+            return "redirect:add";
+        }
+        System.out.println();
+        if (offer.getId() == null) {
+            this.offerService.createOffer(offer);
+        } else {
+            this.offerService.updateOffer(offer);
 
-       ;
+        }
 
-       this.offerService.createOffer(offer);
 
         return "redirect:all";
     }
 
     @GetMapping("/all")
-    public String getAllOffers() {
+    public String getAllOffers(Model model) {
+        model.addAttribute("offers", this.offerService.getOffers());
         return "offers";
+    }
+
+    @GetMapping("/details/{offerId}")
+    public String getDetails(@PathVariable Long offerId,
+                             Model model) {
+
+        model.addAttribute("offer", this.offerService.getOfferById(offerId));
+
+
+        return "details";
+    }
+
+    @PostMapping(value = "/details/{offerId}", params = "offerId")
+    public String updateOffer(@RequestParam Long offerId,
+                              UriComponentsBuilder uriBuilder) {
+        URI uri = uriBuilder.path("/offers/add")
+                .query("mode=edit&offerId={id}").buildAndExpand(offerId).toUri();
+        log.info(uri.toString());
+        return "redirect:" + uri.toString();
+    }
+
+    @GetMapping(value = "/details/{offerId}/delete")
+    public String delete(@PathVariable Long offerId) {
+        this.offerService.deleteOffer(offerId);
+        return "redirect:/оffers/all";
     }
 }
